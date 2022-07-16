84a7f2d2885485c4cb9a3dc0ae4152d159134c4e
pooetitu <pooetitu@gmail.com>
2022-07-16 17:20:08 +0200

    [CC-234]: Fix start being called twice

--- code_runner_server/server.go
+++ code_runner_server/server.go
@@ -2,7 +2,6 @@ package code_runner_server
 
 import (
 	"code-runner/code_runner_server/controllers"
-	"code-runner/code_runner_server/service"
 	"github.com/gin-gonic/gin"
 	"log"
 )
@@ -11,18 +10,8 @@ func StartServer() {
 	engine := gin.Default()
 
 	controllers.SourceControllers(engine)
-	err := service.StartRunner()
-	if err != nil {
-		log.Fatal("unable to start runner on launch", err)
-	}
-	defer func() {
-		err := service.StopRunner()
-		if err != nil {
-			log.Fatal("unable to stop runner", err)
-		}
-	}()
 
-	err = engine.Run(":8080")
+	err := engine.Run(":8080")
 	if err != nil {
 		log.Fatalf("error %s", err)
 	}
--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"code-runner/code_runner_server"
 	"code-runner/code_runner_server/service"
+	"log"
 )
 
 func main() {
-	service.StartRunner()
+	err := service.StartRunner()
+	if err != nil {
+		log.Fatal("unable to start runner on launch", err)
+	}
+	defer func() {
+		err := service.StopRunner()
+		if err != nil {
+			log.Fatal("unable to stop runner", err)
+		}
+	}()
 	code_runner_server.StartServer()
 }
