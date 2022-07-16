ec74c4cfd91bf404df2d8fa6b6048b211647e006
pooetitu <pooetitu@gmail.com>
2022-07-16 21:13:57 +0200

    [CC-234]: Take env port and fix dockerfiles

--- code_runner_server/server.go
+++ code_runner_server/server.go
@@ -2,16 +2,18 @@ package code_runner_server
 
 import (
 	"code-runner/code_runner_server/controllers"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"os"
 )
 
 func StartServer() {
 	engine := gin.Default()
 
 	controllers.SourceControllers(engine)
-
-	err := engine.Run(":8181")
+	port := os.Getenv("RUNNER_PORT")
+	err := engine.Run(fmt.Sprintf(":%s", port))
 	if err != nil {
 		log.Fatalf("error %s", err)
 	}
