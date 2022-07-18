3163f7e0c61aaf1f6c0a78fdf933d73ef05d2a51
pooetitu <pooetitu@gmail.com>
2022-07-18 14:54:23 +0200

    [CC-234]: Add project name log at start

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"code-runner/code_runner_server"
 	"code-runner/code_runner_server/service"
 	"log"
+	"os"
 )
 
 func main() {
+	projectId := os.Getenv("PROJECT_ID")
+	log.Printf("Starting code runner for project: %s\n", projectId)
 	err := service.InstallDependencies()
 	if err != nil {
 		log.Fatal("unable to start runner on launch", err)
