7402d32739fb4160103fd14dac5d86f896709182
pooetitu <pooetitu@gmail.com>
2022-07-18 23:02:40 +0200

    [CC-237]: implement log file watch

--- code_runner_server/server.go
+++ code_runner_server/server.go
@@ -2,8 +2,10 @@ package code_runner_server
 
 import (
 	"code-runner/code_runner_server/controllers"
+	"code-runner/code_runner_server/websockets"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	socketio "github.com/googollee/go-socket.io"
 	"log"
 	"os"
 )
@@ -11,10 +13,30 @@ import (
 func StartServer() {
 	engine := gin.Default()
 
+	server := socketio.NewServer(nil)
+	websockets.SourceWebsockets(server)
+
+	go func() {
+		if err := server.Serve(); err != nil {
+			log.Fatalf("socketio listen error: %s\n", err)
+		}
+	}()
+	defer func(server *socketio.Server) {
+		err := server.Close()
+		if err != nil {
+			log.Fatalf("socketio close error: %s\n", err)
+		}
+	}(server)
+
+	engine.GET("/socket.io/*any", gin.WrapH(server))
+	engine.POST("/socket.io/*any", gin.WrapH(server))
+
 	controllers.SourceControllers(engine)
-	port := os.Getenv("RUNNER_PORT")
-	err := engine.Run(fmt.Sprintf(":%s", port))
-	if err != nil {
+	port := "8080"
+	if value, isPresent := os.LookupEnv("RUNNER_PORT"); isPresent {
+		port = value
+	}
+	if err := engine.Run(fmt.Sprintf(":%s", port)); err != nil {
 		log.Fatalf("error %s", err)
 	}
 }
---
+++ code_runner_server/websockets/websocket.go
@@ -0,0 +1,41 @@
+package websockets
+
+import (
+	"bufio"
+	"code-runner/code_runner_server/service"
+	socketio "github.com/googollee/go-socket.io"
+	"log"
+	"os"
+)
+
+func SourceWebsockets(server *socketio.Server) {
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		log.Println("connected:", s.ID())
+
+		go func() {
+			path := "/logs/" + os.Getenv("PROJECT_ID")
+			f, err := os.Open(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer func(f *os.File) {
+				if err := f.Close(); err != nil {
+					log.Println("Failed to open file")
+				}
+			}(f)
+			service.WatchLogs(path, func() {
+				scanner := bufio.NewScanner(f)
+				line := ""
+				for scanner.Scan() {
+					line += scanner.Text() + "\n"
+				}
+				if line != "" {
+					s.Emit("logs", line)
+				}
+			})
+
+		}()
+		return nil
+	})
+}
