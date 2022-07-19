9d82ac3c92413c6a8f981250e843fa3f6f834d18
pooetitu <pooetitu@gmail.com>
2022-07-19 23:40:27 +0200

    [CC-237]: Prevent duplicate watcher socket

--- code_runner_server/websockets/websocket.go
+++ code_runner_server/websockets/websocket.go
@@ -8,34 +8,41 @@ import (
 	"os"
 )
 
+var connected bool
+
 func SourceWebsockets(server *socketio.Server) {
 	server.OnConnect("/", func(s socketio.Conn) error {
 		s.SetContext("")
-		log.Println("connected:", s.ID())
-
-		go func() {
-			path := "/logs/" + os.Getenv("PROJECT_ID") + ".log"
-			f, err := os.Open(path)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer func(f *os.File) {
-				if err := f.Close(); err != nil {
-					log.Println("Failed to open file")
+		if !connected {
+			log.Println("connected:", s.ID())
+			connected = true
+			go func() {
+				path := "/logs/" + os.Getenv("PROJECT_ID") + ".log"
+				f, err := os.Open(path)
+				if err != nil {
+					log.Fatal(err)
 				}
-			}(f)
-			service.WatchLogs(path, func() {
-				scanner := bufio.NewScanner(f)
-				line := ""
-				for scanner.Scan() {
-					line += scanner.Text() + "\n"
-				}
-				if line != "" {
-					s.Emit("logs", line)
-				}
-			})
-
-		}()
+				defer func(f *os.File) {
+					if err := f.Close(); err != nil {
+						log.Println("Failed to open file")
+					}
+				}(f)
+				service.WatchLogs(path, func() {
+					scanner := bufio.NewScanner(f)
+					line := ""
+					for scanner.Scan() {
+						line += scanner.Text() + "\n"
+					}
+					if line != "" {
+						s.Emit("logs", line)
+					}
+				})
+			}()
+		}
 		return nil
+	})
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		connected = false
+		log.Println("Disconnected:", s.ID())
 	})
 }
