61107b0afc89f8911bfbb553075696b6dd107d86
pooetitu <pooetitu@gmail.com>
2022-07-22 19:52:46 +0200

    [CC-249]: Add status endpoint

--- code_runner_server/controllers/controller.go
+++ code_runner_server/controllers/controller.go
@@ -1,8 +1,14 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func SourceControllers(engine *gin.Engine) {
+	engine.GET("/status", func(context *gin.Context) {
+		context.Writer.WriteHeader(http.StatusOK)
+	})
 	engine.POST("/start", startProject)
 	engine.POST("/restart", restartProject)
 	engine.POST("/dependencies", installDependencies)
