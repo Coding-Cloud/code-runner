e11fac5e057f0166d5d2562052587118c5706188
pooetitu <pooetitu@gmail.com>
2022-07-22 14:26:02 +0200

    [CC-241]: Tee append in file and clear at start

--- code_runners/flask-runner/install-dependencies.sh
+++ code_runners/flask-runner/install-dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pip install -r requirements.txt --target=/libs 2>&1 | tee "/logs/${PROJECT_ID}.log"
+pip install -r requirements.txt --target=/libs 2>&1 | tee -a "/logs/${PROJECT_ID}.log"
--- code_runners/flask-runner/start.sh
+++ code_runners/flask-runner/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python -m flask run --host=${HOST} --port=${PORT} 2>&1 | tee "/logs/${PROJECT_ID}.log"
+python -m flask run --host=${HOST} --port=${PORT} 2>&1 | tee -a "/logs/${PROJECT_ID}.log"
